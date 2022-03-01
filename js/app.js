d8b74836a2aa191e038c14f8338165d118810fc5
azadk53 <mistazad@gmail.com>
2022-03-01 17:28:45 +0100

    others-correction commit

--- js/app.js
+++ js/app.js
@@ -14,6 +14,7 @@ const phoneSearch = () => {
         error.innerText = 'Please give a name';
         search.value = '';
         main.innerHTML = '';
+        phoneDetails.innerHTML = '';
     }
 
     else if (isNaN(searchText)) {
@@ -29,7 +30,7 @@ const phoneSearch = () => {
 
 /* show all products */
 const displayAll = totalPhones => {
-    console.log(totalPhones.length);
+    phoneDetails.innerHTML = '';
     main.innerHTML = '';
     const phones = totalPhones.slice(0, 20); // upto 20 products
 
@@ -39,25 +40,25 @@ const displayAll = totalPhones => {
     else {
         for (const phone of phones) {
             const div = document.createElement('div')  // create div
-            div.className = 'col-lg-4 col-sm-12 mb-4';
+            div.className = 'col-lg-4 col-sm-12 my-4';
             div.innerHTML = `
         <div class="card border border-2 rounded" style="width: 18rem;">
                 <img src="${phone.image}" class=" card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title fw-bold">${phone.phone_name}</h5>
-                    <p class="card-text fw-bold">${phone.brand}</p>
-                    <button onclick="detailsDisplay('${phone.slug}')" class="btn btn-primary">Details</button>
+                    <p class="card-text fw-bold">${phone.brand}</p>   
+                    <button onclick="detailsDisplay('${phone.slug}')" class="btn btn-primary">Details</button>                    
                 </div>
             </div>
         `
             main.appendChild(div);   // add div
         }
     }
-
 }
 
 /* show details */
 const detailsDisplay = (id) => {
+    /*  window.scrollto(0, 0) */
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
         .then(res => res.json())
@@ -69,21 +70,13 @@ const detailsDisplay = (id) => {
             for (let i = 0; i < details.sensors.length; i++) {
                 temp += details.sensors[i] + ' ';
             }
-
-            /* others loop */
-            const othersString = data.data.others;
-            for (var property in othersString) {
-                console.log(`${property}: ${othersString[property]}`);
-            }
-            /*  main.innerHTML = ""; */
-
             const div = document.createElement('div');
             phoneDetails.innerHTML = '';
             div.className = 'my-4';
             div.innerHTML = `
             <div class="card border border-2 rounded mx-auto" style="width: 18rem;">
             <img src="${data.data.image}" class=" card-img-top" alt="...">
-            <div class="card-body">   
+            <div id="card-body" class="card-body">   
                 <h5 class="card-title">${data.data.name}</h5>
                 <p class="card-text fw-bold">${data.data.brand}</p>
                 <p class="card-text fw-bold">${data.data.releaseDate}</p>
@@ -91,14 +84,24 @@ const detailsDisplay = (id) => {
                 <p class="card-text"><span class="fw-bold">Display: </span> ${details.displaySize}</p>
                 <p class="card-text"><span class="fw-bold">Chipset: </span>${details.chipSet}</p>
                 <p class="card-text"><span class="fw-bold">Memory: </span> ${details.memory}</p>
-                <p class="card-text"><span class="fw-bold">Sensors: </span> ${temp}</p>
-                
-                     
+                <p class="card-text"><span class="fw-bold">Sensors: </span> ${temp}</p>                    
             </div>
         </div>
     `
             phoneDetails.appendChild(div);
+
+            /* others loop */
+            var othersString = data?.data?.others;
+            console.log(typeof (othersString))
+            if (typeof (othersString) !== undefined) {
+                for (var property in othersString) {
+                    console.log(`${property}: ${othersString[property]}`);
+                }
+                const cardBody = document.getElementById('card-body');
+                const p = document.createElement('p');
+                p.className = 'card-text';
+                p.innerHTML = `<span class="fw-bold">Others:</span> ${property}: ${othersString[property]}`
+                cardBody.appendChild(p);
+            }
         })
 }
-
-/* <p class="card-text"><span class="fw-bold">Sensors:</span> ${property}: ${othersString[property]}</p> */
